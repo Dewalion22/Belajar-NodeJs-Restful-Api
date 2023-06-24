8ab5af12697493d71aea5bfc605e93fdc3fc1828
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-06-24 11:15:52 +0700

    register user

---
+++ src/route/api.js
@@ -0,0 +1,17 @@
+import exports  from "express"
+import userController from "../controller/user-controller.js"
+import express from "express";
+import {authMiddleware} from "../middleware/auth-middleware.js";
+
+
+const userRouter = new express.Router();
+userRouter.use(authMiddleware);
+
+// User API
+//userRouter.get('/api/users/current', userController.get)
+
+
+
+export {
+    userRouter
+}
