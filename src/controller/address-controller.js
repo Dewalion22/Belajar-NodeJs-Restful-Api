5b1d7903fe912a48551b67e133828d3799cf98f1
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-06-29 20:04:59 +0700

    address update

--- src/controller/address-controller.js
+++ src/controller/address-controller.js
@@ -32,7 +32,36 @@ const get = async (req, res, next) => {
     }
 }
 
+const  update = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const contactId = req.params.contactId;
+        const addressId = req.params.addressId;
+        const request = req.body;
+        request.id = addressId;
+
+        const result = await addressService.update(user, contactId, request);
+
+        res.status(200).json({
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+const  remove = async (req, res, next) => {
+
+}
+
+const  list = async (req, res, next) => {
+
+}
+
 export default {
     create,
-    get
+    get,
+    update,
+    remove,
+    list
 }
